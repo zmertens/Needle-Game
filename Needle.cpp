12c8a9fbdc08968b91b3a5d99185a88ae95219f6
zmertens <emptyseth0@gmail.com>
2021-11-13 12:48:59 -0700

    Add libzep to libs \

--- Needle.cpp
+++ Needle.cpp
@@ -1,5 +1,14 @@
 #include "Needle.hpp"
 
+// Zep libraries
+#include <zep/imgui/display_imgui.h>
+#include <zep/imgui/editor_imgui.h>
+#include <zep/mode_standard.h>
+#include <zep/mode_vim.h>
+#include <zep/theme.h>
+#include <zep/tab_window.h>
+#include <zep/window.h>
+
 /**
  * Statics
  */
@@ -46,6 +55,122 @@ int Needle::doStuff()
 #endif 
     }
 
+    // Zep variables and fields
+    const std::string shader = R"R(
+    #version 330 core
+
+    uniform mat4 Projection;
+
+    // Coordinates  of the geometry
+    layout(location = 0) in vec3 in_position;
+    layout(location = 1) in vec2 in_tex_coord;
+    layout(location = 2) in vec4 in_color;
+
+    // Outputs to the pixel shader
+    out vec2 frag_tex_coord;
+    out vec4 frag_color;
+
+    void main()
+    {
+        gl_Position = Projection * vec4(in_position.xyz, 1.0);
+        frag_tex_coord = in_tex_coord;
+        frag_color = in_color;
+    }
+    )R";
+    // A helper struct for Zep to init the editor and handle callbacks
+//     struct ZepContainer : public IZepComponent
+//     {
+//         ZepContainer(const std::string& startupFilePath)
+//             : spEditor(std::make_unique<ZepEditor_ImGui>(ZEP_ROOT))
+//         {
+
+//             // File watcher not used on apple yet ; needs investigating as to why it doesn't compile/run 
+// #ifndef __APPLE__
+// MUtils::Watcher::Instance().AddWatch(ZEP_ROOT, [&](const ZepPath & path)
+// {
+//     spEditor->OnFileChanged(path);
+// }, false);
+// #endif
+
+//             spEditor->RegisterCallback(this);
+
+//             float ddpi = 0.0f;
+//             float hdpi = 0.0f;
+//             float vdpi = 0.0f;
+//             auto res = SDL_GetDisplayDPI(0, &ddpi, &hdpi, &vdpi);
+//             if (res == 0 && hdpi != 0)
+//             {
+//                 spEditor->SetPixelScale(hdpi / 96.0f);
+//             }
+
+//             if (!startupFilePath.empty())
+//             {
+//                 spEditor->InitWithFileOrDir(startupFilePath);
+//             }
+//             else
+//             {
+//                 spEditor->InitWithText("Shader.vert", shader);
+//             }
+//         }
+
+//         ZepContainer()
+//         {
+//             spEditor->UnRegisterCallback(this);
+//         }
+
+//         // Inherited via IZepComponent
+//         virtual void Notify(std::shared_ptr<ZepMessage> message) override
+//         {
+//             if (message->messageId == Msg::Tick)
+//             {
+// #ifndef __APPLE__
+// MUtils::Watcher::Instance().Update();
+// #endif
+//             }
+//             else if (message->messageId == Msg::Quit)
+//             {
+//                 quit = true;
+//             }
+//             else if (message->messageId == Msg::ToolTip)
+//             {
+//                 auto spTipMsg = std::static_pointer_cast<ToolTipMessage>(message);
+//                 if (spTipMsg->location != -1l && spTipMsg->pBuffer)
+//                 {
+//                     auto pSyntax = spTipMsg->pBuffer->GetSyntax();
+//                     if (pSyntax)
+//                     {
+//                         if (pSyntax->GetSyntaxAt(spTipMsg->location) == ThemeColor::Identifier)
+//                         {
+//                             auto spMarker = std::make_shared<RangeMarker>();
+//                             spMarker->description = "This is an identifier";
+//                             spMarker->highlightColor = ThemeColor::Identifier;
+//                             spMarker->textColor = ThemeColor::Text;
+//                             spTipMsg->spMarker = spMarker;
+//                             spTipMsg->handled = true;
+//                         }
+//                         else if (pSyntax->GetSyntaxAt(spTipMsg->location) == ThemeColor::Keyword)
+//                         {
+//                             auto spMarker = std::make_shared<RangeMarker>();
+//                             spMarker->description = "This is a keyword";
+//                             spMarker->highlightColor = ThemeColor::Keyword;
+//                             spMarker->textColor = ThemeColor::Text;
+//                             spTipMsg->spMarker = spMarker;
+//                             spTipMsg->handled = true;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+
+//         virtual ZepEditor& GetEditor() const override
+//         {
+//             return *spEditor;
+//         }
+
+//         bool quit = false;
+//         std::unique_ptr<ZepEditor_ImGui> spEditor;
+//     };
+//     ZepContainer zep ("");
 
     ImVec4 clearColor = ImVec4(0.35f, 0.55f, 0.71f, 1.0f);
 #if defined(NEEDLE_DEBUG)
@@ -97,8 +222,29 @@ int Needle::doStuff()
         glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // render
-        // ------
+        // This is a bit messy; and I have no idea why I don't need to remove the menu fixed_size from the calculation!
+        int w, h, displayMode;
+        glfwGetFramebufferSize(mGlfwHandler.getGlfwWindow(), &w, &h);
+        auto menuSize = ImGui::GetStyle().FramePadding.y * 2 + ImGui::GetFontSize();
+        ImGui::SetNextWindowPos(ImVec2(0, menuSize));
+        ImGui::SetNextWindowSize(ImVec2(float(w), float(h))); // -menuSize)));
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 0.0f);
+        ImGui::Begin("Zep", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar /*| ImGuiWindowFlags_NoScrollbar*/);
+        ImGui::PopStyleVar(4);
+
+        // 'hide' the window contents from ImGui, so it doesn't try dragging when we move our scrollbar, etc.
+        ImGui::InvisibleButton("ZepContainer", ImGui::GetWindowSize());
+
+        // // TODO: Change only when necessray
+        // zep.spEditor->SetDisplayRegion(toNVec2f(ImGui::GetWindowPos()), toNVec2f(ImGui::GetWindowSize()));
+
+        // // Display the editor inside this window
+        // zep.spEditor->Display();
+        // zep.spEditor->HandleInput();
 
         mGlTriangle->draw();
         ImGui::Render();
---
+++ libs/zep/zep/list.cmake
@@ -0,0 +1,71 @@
+
+SET(ZEP_ROOT ${CMAKE_CURRENT_LIST_DIR}/)
+SET(ZEP_SOURCE
+${ZEP_ROOT}/../m3rdparty/animation/timer.cpp
+${ZEP_ROOT}/../m3rdparty/animation/timer.h
+${ZEP_ROOT}/../m3rdparty/string/stringutils.cpp
+${ZEP_ROOT}/../m3rdparty/string/stringutils.h
+${ZEP_ROOT}/../m3rdparty/threadpool/threadutils.h
+${ZEP_ROOT}/../m3rdparty/file/archive.h
+${ZEP_ROOT}/../m3rdparty/file/archive.cpp
+${ZEP_ROOT}/../m3rdparty/file/path.h
+${ZEP_ROOT}/../m3rdparty/file/path.cpp
+${ZEP_ROOT}/../m3rdparty/logger.h
+${ZEP_ROOT}/filesystem.cpp
+${ZEP_ROOT}/filesystem.h
+${ZEP_ROOT}/editor.cpp
+${ZEP_ROOT}/editor.h
+${ZEP_ROOT}/splits.cpp
+${ZEP_ROOT}/splits.h
+${ZEP_ROOT}/buffer.cpp
+${ZEP_ROOT}/buffer.h
+${ZEP_ROOT}/commands.cpp
+${ZEP_ROOT}/commands.h
+${ZEP_ROOT}/display.cpp
+${ZEP_ROOT}/display.h
+${ZEP_ROOT}/tab_window.cpp
+${ZEP_ROOT}/tab_window.h
+${ZEP_ROOT}/window.cpp
+${ZEP_ROOT}/window.h
+${ZEP_ROOT}/scroller.cpp
+${ZEP_ROOT}/scroller.h
+${ZEP_ROOT}/syntax.cpp
+${ZEP_ROOT}/syntax.h
+${ZEP_ROOT}/syntax_providers.cpp
+${ZEP_ROOT}/syntax_providers.h
+${ZEP_ROOT}/syntax_rainbow_brackets.cpp
+${ZEP_ROOT}/syntax_rainbow_brackets.h
+${ZEP_ROOT}/mode.cpp
+${ZEP_ROOT}/mode.h
+${ZEP_ROOT}/mode_standard.cpp
+${ZEP_ROOT}/mode_standard.h
+${ZEP_ROOT}/mode_vim.cpp
+${ZEP_ROOT}/mode_vim.h
+${ZEP_ROOT}/mode_search.cpp
+${ZEP_ROOT}/mode_search.h
+${ZEP_ROOT}/theme.cpp
+${ZEP_ROOT}/theme.h
+${ZEP_ROOT}/list.cmake
+)
+
+LIST(APPEND SRC_INCLUDE ${ZEP_ROOT}/src ${ZEP_ROOT}/../)
+
+# IF (BUILD_QT)
+# SET(ZEP_SOURCE_QT
+#     ${ZEP_ROOT}/src/qt/zepdisplay_qt.cpp
+#     ${ZEP_ROOT}/src/qt/zepdisplay_qt.h
+#     ${ZEP_ROOT}/src/qt/zepwidget_qt.cpp
+#     ${ZEP_ROOT}/src/qt/zepwidget_qt.h
+#     )
+# SET(ZEP_INCLUDE_QT ${ZEP_ROOT}/src/qt)
+# ENDIF()
+
+# IF (BUILD_IMGUI)
+# SET(ZEP_SOURCE_IMGUI
+#     ${ZEP_ROOT}/src/imgui/display_imgui.cpp
+#     ${ZEP_ROOT}/src/imgui/display_imgui.h
+#     ${ZEP_ROOT}/src/imgui/editor_imgui.cpp
+#     ${ZEP_ROOT}/src/imgui/editor_imgui.h)
+# SET(ZEP_INCLUDE_IMGUI ${ZEP_ROOT}/src/qt)
+# ENDIF()
+
